508de6539f714a50153c1b5c776e2642e37a6e44
Unknown <dlalsdk12321@naver.com>
2018-10-25 14:42:13 +0900

    스프링 - 페이지네이션을 위한 Criteria클래스 생성

---
+++ src/main/java/kr/green/spring/pagenation/Criteria.java
@@ -0,0 +1,39 @@
+package kr.green.spring.pagenation;
+
+public class Criteria {
+//	한 페이지당 보여지는 컨텐츠 수(게시글 수)
+	private int perPageNum;
+//	현재 페이지
+	private int page;
+	
+	public int getPerPageNum() {
+		return perPageNum;
+	}
+	public int getPage() {
+		return page;
+	}
+	public void setPerPageNum(int perPageNum) {
+		/*	한페이지당 보여지는 게시글이 최소 5개
+		 *  이상이 되도록 설정 */
+		if(perPageNum <=4)
+			this.perPageNum = 5;
+		this.perPageNum = perPageNum;
+	}
+	public void setPage(int page) {
+		/* 현재 페이지가 1페이지 이상이 되도록 설정*/
+		if(page <=0)
+			this.page = 1;
+		else
+			this.page = page;
+	}
+	/* 쿼리문에서 limit를 사용할 떄 가져올 컨텐츠의 시작 위치를
+	 * 계산 하는 메소드 xml에서 #{startPage}를 호출할때
+	 * getStartPage가 호출됨 */
+	public int getStartPage() {
+		return (page-1)*perPageNum;
+	}
+	public Criteria() {
+		page = 1;
+		perPageNum = 10;
+	}
+}
